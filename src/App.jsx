50227984f58015c9a260f69ba74d6b429cfe5633
Diksha0032 <chaurasiyadiksha11@gmail.com>
2025-09-25 21:25:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-white text-black text-1xl">
+       <h1>yeyeye</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
